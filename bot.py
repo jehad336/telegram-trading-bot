c0b8ba87f914586a471ec3b3c405c4c4c48cb7f6
jehad336 <ph.jehad14@gmail.com>
2026-01-04 13:39:33 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,13 +6,13 @@ TOKEN = os.getenv("8230458260:AAEeVWX0lxf3gR1YKpN_LRdl5gXil3w8A_4")
 CHANNEL_ID = os.getenv("mmmesh1")
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
     if update.message.photo:
         await context.bot.send_photo(
             chat_id=CHANNEL_ID,
             photo=update.message.photo[-1].file_id,
             caption=update.message.caption
         )
-   
     elif update.message.text:
         await context.bot.send_message(
             chat_id=CHANNEL_ID,
