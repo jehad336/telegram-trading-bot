b7831e97596b1bbb5d2e46cb9ac5c890d8041d80
jehad336 <ph.jehad14@gmail.com>
2026-01-04 14:21:04 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = "8230458260:AAEeVWX0lxf3gR1YKpN_LRdl5gXil3w8A_4"
 CHANNEL_ID = "@mmmesh1"
 
-async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+async def forward_to_channel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.photo:
+        photo = update.message.photo[-1].file_id
+        caption = update.message.caption
         await context.bot.send_photo(
             chat_id=CHANNEL_ID,
-            photo=update.message.photo[-1].file_id,
-            caption=update.message.caption
+            photo=photo,
+            caption=caption
         )
+
     elif update.message.text:
         await context.bot.send_message(
             chat_id=CHANNEL_ID,
@@ -20,5 +21,5 @@ async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.ALL, forward))
+app.add_handler(MessageHandler(filters.ALL, forward_to_channel))
 app.run_polling()
